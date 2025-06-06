897101e6b13ad5b69417f976dee0539fce1f840e
KonstantinosLamprakis <28678729+KonstantinosLamprakis@users.noreply.github.com>
2025-06-06 07:04:12 +0200

    Create lint rules for naming conversions

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,7 +1,7 @@
 module.exports = {
     root: true,
     parser: '@typescript-eslint/parser',
-    plugins: ['@typescript-eslint', 'prettier'],
+    plugins: ['@typescript-eslint', 'prettier', 'filenames'],
     extends: [
         'eslint:recommended',
         'plugin:@typescript-eslint/recommended',
@@ -12,8 +12,68 @@ module.exports = {
         sourceType: 'module',
     },
     rules: {
-        // your custom rules here
         'prettier/prettier': ['error'],
+
+        'filenames/match-regex': [2, '^[a-z0-9-]+$', true], // kebab-case
+
+        '@typescript-eslint/naming-convention': [
+            'error',
+
+            // Constants: UPPER_CASE
+            {
+                selector: 'variable',
+                modifiers: ['const'],
+                format: ['UPPER_CASE'],
+                filter: {
+                    regex: '^(CN_.*|MIN_.*|MAX_.*|DEFAULT_.*|CONFIG_.*|VERSION)$',
+                    match: true
+                }
+            },
+
+            // Regular const variables (not true constants), let and var as well
+            {
+                selector: 'variable',
+                format: ['camelCase'],
+                filter: {
+                    regex: '^(?!CN_.*|MIN_.*|MAX_.*|DEFAULT_.*|CONFIG_.*|VERSION)',
+                    match: true
+                }
+            },
+
+            // Functions
+            {
+                selector: 'function',
+                format: ['camelCase']
+            },
+
+            // Classes, Types, Interfaces
+            {
+                selector: 'typeLike',
+                format: ['PascalCase']
+            },
+
+            // Interfaces (optional: disallow `I` prefix)
+            {
+                selector: 'interface',
+                format: ['PascalCase'],
+                custom: {
+                    regex: '^I[A-Z]',
+                    match: false
+                }
+            },
+
+            // Enums
+            {
+                selector: 'enum',
+                format: ['PascalCase']
+            },
+
+            // Enum members
+            {
+                selector: 'enumMember',
+                format: ['UPPER_CASE']
+            },
+        ]
     },
     ignorePatterns: ['dist/', 'node_modules/'],
 }
