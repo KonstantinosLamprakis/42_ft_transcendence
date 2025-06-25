298a3d8180f878f2cd8492a782e8a2fe0fd9fcd5
KonstantinosLamprakis <28678729+KonstantinosLamprakis@users.noreply.github.com>
2025-06-25 22:38:01 +0200

    Update sqlite-db to ESM, add Makefile, migrate to new structure

--- services/sqlite-db/src/server.ts
+++ services/sqlite-db/src/server.ts
@@ -1,9 +1,31 @@
 import Fastify from "fastify";
 import Database from "better-sqlite3";
+import * as dotenv from "dotenv";
+import { existsSync, mkdirSync } from "fs";
+import * as path from "path";
+import { fileURLToPath } from "url";
+
+dotenv.config();
+
+enum Runtime {
+	LOCAL = "local",
+	DOCKER = "docker",
+}
 
 const fastify = Fastify({ logger: true });
 
-const db = new Database("/data/database.db");
+if (process.env.RUNTIME === Runtime.LOCAL) {
+	const dirname = path.dirname(fileURLToPath(import.meta.url));
+	// const uploadsDir = path.join(dirname, "data");
+	const uploadsDir = "./data";
+	if (!existsSync(uploadsDir)) {
+		mkdirSync(uploadsDir, { recursive: true });
+	}
+}
+
+const db = new Database(
+	process.env.RUNTIME === Runtime.LOCAL ? "./data/database.db" : "/data/database.db",
+);
 
 db.exec(`
   CREATE TABLE IF NOT EXISTS users (
