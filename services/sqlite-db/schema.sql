0395496358ca19e92af047f1b19706d6edf4b24a
KonstantinosLamprakis <28678729+KonstantinosLamprakis@users.noreply.github.com>
2025-06-29 17:58:51 +0200

    Add Google integration for sign in

--- services/sqlite-db/schema.sql
+++ services/sqlite-db/schema.sql
@@ -4,8 +4,9 @@ CREATE TABLE
         username TEXT UNIQUE NOT NULL,
         password TEXT NOT NULL,
         name TEXT NOT NULL,
-        avatar TEXT, -- store the filename of the uploaded image
+        avatar TEXT, -- store the filename or URL of the uploaded image
         wins INTEGER DEFAULT 0,
         loses INTEGER DEFAULT 0,
-        twofa_secret TEXT
+        twofa_secret TEXT,
+        isGoogleAccount BOOLEAN DEFAULT 0
     );
